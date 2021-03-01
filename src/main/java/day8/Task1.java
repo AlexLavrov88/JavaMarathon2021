07531956af41ac0be45e7eca53d2567d96feedf5
Александр <Lavrov_alex@list.ru>
2021-03-01 21:56:59 +0300

    complete day8 task

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,27 @@
 package day8;
 
+import java.sql.Time;
+
 public class Task1 {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        String string = 0 + " ";
+        for (int i = 1; i < 20001; i++) {
+            string = string + i + " ";
+        }
+        System.out.println(string);
+        long finish = System.currentTimeMillis();
+        long howMuchTime = finish - start;
+        System.out.println(howMuchTime);
 
+        StringBuilder sb = new StringBuilder();
+        long startSb = System.currentTimeMillis();
+        for (int i = 0; i < 20001; i++) {
+            sb = sb.append(i + " ");
+        }
+        long finishSb = System.currentTimeMillis();
+        long howMuchTimeSb = finishSb - startSb;
+       System.out.println(sb);
+        System.out.println(howMuchTimeSb);
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,11 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane airplane = new Airplane ("Boeing", 2000, 150, 10000);
+        System.out.println(airplane );
     }
+
+//    public String toString() {
+//////        return  "Изготовитель: " + this.producer + ", год выпуска: " + this.year + ", длина: " + this.lenght + ", вес: " + this.weight + ", количество топлива в баке: " + this.fuel
+//////    }
 }
