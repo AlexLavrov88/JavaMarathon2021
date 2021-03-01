6a2af122946f6b8fac26fab1e03bf61181a033be
Александр <Lavrov_alex@list.ru>
2021-03-01 22:14:00 +0300

    complete day8 tasks ver 1

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -9,19 +9,19 @@ public class Task1 {
         for (int i = 1; i < 20001; i++) {
             string = string + i + " ";
         }
-        System.out.println(string);
         long finish = System.currentTimeMillis();
+        System.out.println(string);
         long howMuchTime = finish - start;
         System.out.println(howMuchTime);
 
         StringBuilder sb = new StringBuilder();
         long startSb = System.currentTimeMillis();
         for (int i = 0; i < 20001; i++) {
-            sb = sb.append(i + " ");
+            sb = sb.append(i);
+            sb = sb.append(" ");
         }
         long finishSb = System.currentTimeMillis();
         long howMuchTimeSb = finishSb - startSb;
-       System.out.println(sb);
         System.out.println(howMuchTimeSb);
     }
 }
